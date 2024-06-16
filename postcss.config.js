c36351bb5b2f07430c092b7283a659fa9ed1b6e3
Divinesoumyadip <soumyadipdasmahapatra343@gmail.com>
2024-06-16 23:30:51 +0530

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
